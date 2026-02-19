25370e60f19571de63aa6d29c59cad0f5256ef26
Михаил Ольховиков <miholxovikov92@mail.ru>
2026-02-19 09:17:47 +0500

    V6

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -2,6 +2,7 @@ from cats.views import AchievementViewSet, CatViewSet
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 from rest_framework import routers
 
@@ -9,11 +10,17 @@ router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)
 router.register(r'achievements', AchievementViewSet)
 
+
+def health_check(request):
+    return HttpResponse("OK")
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('api/', include('djoser.urls')),  # Работа с пользователями
-    path('api/', include('djoser.urls.authtoken')),  # Работа с токенами
+    path("health/", health_check),
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
+    path("api/", include("djoser.urls")),  # Работа с пользователями
+    path("api/", include("djoser.urls.authtoken")),  # Работа с токенами
 ]
 
 if settings.DEBUG:
